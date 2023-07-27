0f778be240eb1dc2371ff0410262e1be084fd08d
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2023-07-27 19:42:16 -0300

    Segunda alteração

--- FrostMixNewCode2905.ino
+++ FrostMixNewCode2905.ino
@@ -5,10 +5,6 @@
 SoftwareSerial serialDoBluetooth(8,9);
 
 Servo servo_01;
-//Servo servo_02;
-
-
-// Declarar as váriaveis das válvulas e intervalos
 
 int valve3 = 3;
 int valve4 = 4;
@@ -23,9 +19,7 @@ void setup()
   serialDoBluetooth.begin(9600);
 
   servo_01.attach(6);
-  //servo_02.attach(6);
   
-
   pinMode(valve3, OUTPUT);
   pinMode(valve4, OUTPUT);
   pinMode(motor, OUTPUT);
@@ -58,16 +52,11 @@ void loop()
       servo_01.write(45);
       delay(2000);    
       servo_01.write(0);
-    
 
       digitalWrite(motor, HIGH);
       delay(2000);
       digitalWrite(motor, LOW);
-    
-      //servo_02.write(45);
-      //delay(2000);
-    
-      //servo_02.write(0);
+
     
       digitalWrite(valve3, HIGH);
       delay(2000);
